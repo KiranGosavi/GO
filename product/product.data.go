8607acc67527c959b219e436c0e4362f3b9b5aa2
gokirans@gmail.com <kg@4fo.de>
2020-10-28 20:13:03 +0100

    Cleaning code

--- product/product.data.go
+++ product/product.data.go
@@ -8,49 +8,7 @@ import (
 	"github.com/KiranGosavi/webservice/database"
 )
 
-// var productMap = struct {
-// 	sync.RWMutex
-// 	m map[int]Product
-// }{m: make(map[int]Product)}
 
-// func init() {
-// 	fmt.Println("loading products >>>>>>>>")
-// 	prodMap, err := loadProductMap()
-// 	productMap.m = prodMap
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Printf("%d products loaded...\n", len(productMap.m))
-// }
-
-// func loadProductMap() (map[int]Product, error) {
-// 	filename := "products.json"
-// 	_, err := os.Stat(filename)
-// 	if os.IsNotExist(err) {
-// 		return nil, fmt.Errorf("file[%s] does not exists", filename)
-// 	}
-
-// 	file, _ := ioutil.ReadFile(filename)
-// 	productList := make([]Product, 0)
-// 	err = json.Unmarshal([]byte(file), &productList)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	prodMap := make(map[int]Product)
-// 	for i := 0; i < len(productList); i++ {
-// 		prodMap[productList[i].ProductID] = productList[i]
-// 	}
-// 	return prodMap, nil
-// }
-
-// func getProduct(productID int) *Product {
-// 	productMap.RLock()
-// 	defer productMap.RUnlock()
-// 	if product, ok := productMap.m[productID]; ok {
-// 		return &product
-// 	}
-// 	return nil
-// }
 func getProduct(productID int) (*Product, error) {
 	//fmt.Println(productID)
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
@@ -81,15 +39,6 @@ func getProduct(productID int) (*Product, error) {
 	return product, nil
 }
 
-// func getProductList() []Product {
-// 	productMap.RLock()
-// 	products := make([]Product, 0, len(productMap.m))
-// 	for _, value := range productMap.m {
-// 		products = append(products, value)
-// 	}
-// 	productMap.RUnlock()
-// 	return products
-// }
 
 //[{"productId":1,"manufacturer":"Johns-Jenkins","sku":"p5z343vdS","upc":"939581000000","pricePerUnit":"497.45","quantityOnHand":9703,"productName":"sticky note"}
 func getProductList() ([]Product, error) {
@@ -159,11 +108,6 @@ func GetTopTenProducts() ([]Product, error) {
 	return products, nil
 }
 
-// func removeProduct(productID int) {
-// 	productMap.Lock()
-// 	defer productMap.Unlock()
-// 	delete(productMap.m, productID)
-// }
 func removeProduct(productID int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
@@ -174,43 +118,7 @@ func removeProduct(productID int) error {
 	return nil
 }
 
-// func getProductIDs() []int {
-// 	productMap.RLock()
-// 	productIDs := []int{}
-// 	for key := range productMap.m {
-// 		productIDs = append(productIDs, key)
-// 	}
-// 	productMap.RUnlock()
-// 	sort.Ints(productIDs)
-// 	return productIDs
-// }
 
-// func getNextProductID() int {
-// 	productIDS := getProductIDs()
-// 	return productIDS[len(productIDS)-1] + 1
-// }
-
-// func addOrUpdateProduct(product Product) (int, error) {
-// 	addOrUpdateID := -1
-// 	if product.ProductID > 0 {
-// 		oldProduct, err := getProduct(product.ProductID)
-// 		if err != nil {
-// 			return addOrUpdateID, err
-// 		}
-// 		if oldProduct == nil {
-// 			return 0, fmt.Errorf("product id %g doen not exist", product.ProductID)
-// 		}
-// 		addOrUpdateID = product.ProductID
-// 	} else {
-// 		addOrUpdateID = getNextProductID()
-// 		product.ProductID = addOrUpdateID
-// 	}
-// 	productMap.Lock()
-// 	productMap.m[addOrUpdateID] = product
-// 	productMap.Unlock()
-// 	return addOrUpdateID, nil
-
-// }
 func updateProduct(product Product) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
